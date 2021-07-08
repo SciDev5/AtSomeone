92776034aa2ad3ff69f83539699c6715fff6c475
SciDev5 <67175934+SciDev5@users.noreply.github.com>
2021-07-07 21:45:46 -0400

    Make sure sender & bot arent mentioned by @someone

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ client.on("message",async message => {
     console.log("Message [by: %s]: %s",message.author.tag,message.content);
     if (message.mentions.users.find((_user,id) => id === client.user.id)) {
         console.log("'@someone' Mentioned, Forwarding...");
-        var members = message.guild.members.cache || await message.guild.members.fetch();
-        await message.channel.send(message.content.replace(new RegExp(`<@!${client.user.id}>`,"g"),()=>`<@!${members.random().id}>`));
+        var members = (message.guild.members.cache || await message.guild.members.fetch()).filter(v => v.id !== message.author.id && v.id !== client.user.id);
+        await message.channel.send(`[From <@!${message.author.id}>] `+message.content.replace(new RegExp(`<@!${client.user.id}>`,"g"),()=>`<@!${members.random().id}>`));
     }
     if (message.content.startsWith("^s")) {
         client.destroy();
